571db6cca8701f72ca1056bed00a0830fe1bdee4
Dev Valladares <devalladares@gmail.com>
2021-04-11 18:16:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -60,12 +60,12 @@ function init() {
 	 * Loading
 	 */
 	const environmentMap = cubeTextureLoader.load([
-		'../textures/environmentMaps/0/px.jpg',
-		'../textures/environmentMaps/0/nx.jpg',
-		'../textures/environmentMaps/0/py.jpg',
-		'../textures/environmentMaps/0/ny.jpg',
-		'../textures/environmentMaps/0/pz.jpg',
-		'../textures/environmentMaps/0/nz.jpg'
+		'./textures/environmentMaps/0/px.jpg',
+		'./textures/environmentMaps/0/nx.jpg',
+		'./textures/environmentMaps/0/py.jpg',
+		'./textures/environmentMaps/0/ny.jpg',
+		'./textures/environmentMaps/0/pz.jpg',
+		'./textures/environmentMaps/0/nz.jpg'
 	])
 
 	environmentMap.encoding = THREE.sRGBEncoding
