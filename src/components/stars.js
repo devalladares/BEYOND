24ba072ff5d0cbc3b91e86649ca080eccbef84dd
Dev Valladares <devalladares@gmail.com>
2021-04-16 10:43:25 -0400

    Create stars.js

---
+++ src/components/stars.js
@@ -0,0 +1,56 @@
+import {
+	SphereGeometry,
+	PointsMaterial,
+	Vector3,
+	Points,
+	Object3D,
+	BufferGeometry,
+	BufferAttribute
+} from '../../build/three.module.js';
+
+function createStars(scene, starGroup) {
+
+	const starQty = 24000 / 4;
+	const starGeometry = new BufferGeometry()
+
+	const materialOptions = {
+		size: 1.0,
+		fog: false,
+		color: 'white'
+	};
+
+	const positionArray = new Float32Array(starQty * 3)
+	const scaleArray = new Float32Array(starQty)
+
+	for (let i = 0; i < starQty; i++) {
+		positionArray[i * 3 + 0] = Math.random() * 4
+		positionArray[i * 3 + 1] = Math.random() * 4
+		positionArray[i * 3 + 2] = Math.random() * 4
+
+		scaleArray[i] = Math.random()
+
+	}
+
+	starGeometry.setAttribute('aScale', new BufferAttribute(scaleArray, 1))
+	starGeometry.setAttribute('position', new BufferAttribute(positionArray, 3))
+
+	const starStuff = new PointsMaterial(materialOptions);
+
+	for (let i = 0; i < starQty; i++) {
+		positionArray[i * 3 + 0] = (Math.random() - 0.5) * 3000
+		positionArray[i * 3 + 1] = Math.random() * 2000 + 250
+		positionArray[i * 3 + 2] = (Math.random() - 0.5) * 3000
+	}
+
+	const stars = new Points(starGeometry, starStuff);
+
+	starGroup = new Object3D();
+	starGroup.add(stars)
+	scene.add(starGroup);
+
+	return starGroup;
+}
+
+export {
+	createStars
+};
